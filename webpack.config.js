3708525bbfed251e2e14404074c386bff80bc40e
Vidar Aune Westrum <vidar@dolittle.com>
2018-01-15 15:21:37 +0100

    Handles HTML partials, inline SVGs and scss files

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,27 @@ const path = require('path');
 const configParts = require("./webpack.parts");
 
 
-module.exports = {
-    entry: ['./src/scripts/index.js', './src/index.html'],
+module.exports = [{
+    entry: ['./src/scripts/index.js', './src/styles/style.scss'],
     output: {
         filename: 'bundle.[hash:9].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{
-            test: /\.html$/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]'
-                    }
-                },
-                {
-                    loader: 'extract-loader'
-                },
-                {
-                    loader: 'html-loader',
-                    options: {
-                        attrs: ["img:src", "link:href"],
-                        interpolate: true,
-                    }
-                }],
-        }]
+        rules: [
+            configParts.loaders.htmlLoader,
+            configParts.loaders.sassLoader,
+            configParts.loaders.svgLoader
+        ]
     },
     plugins: [
-        configParts.plugins.cleanDistFolderAndIndexfile
-    ]
-};
+        configParts.plugins.cleanDistFolderAndIndexfile,
+        configParts.plugins.buildHtmlIndex,
+        configParts.plugins.sassBuilder
+    ],
+    devServer: {
+        contentBase: './dist',
+        port: 1337,
+        open: true
+    }
+}];
--- webpack.parts.js
+++ webpack.parts.js
@@ -1,16 +1,60 @@
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+let sassBuilder = new ExtractTextPlugin({
+    filename: "style.bundle.[hash:9].css",
+    allChunks: true
+});
 
 module.exports = {
-    loaders:{
-        htmlLoader:{
-            test: /\.html&/,
-            use:{
-                loader: 'html-loader',
-                options: {}
+    loaders: {
+        htmlLoader: {
+            test: /\.html$/,
+            exclude: /index.html/,
+            use: [
+                {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: ["img:src", "link:href"],
+                        interpolate: true,
+                    }
+                }],
+        },
+        sassLoader: {
+            test: /\.scss$/,
+            loader: sassBuilder.extract([
+                "css-loader",
+                "sass-loader",
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        plugins: () => ([
+                            require("autoprefixer")()
+                        ])
+                    }
+                }])
+        },
+        svgLoader: {
+            test: /\.svg/,
+            use: {
+                loader: 'svg-inline-loader',
+                options: {
+                    removeTags: true,
+                    removingTags: ['title', 'desc'],
+                    removingTagAttrs: ['id', 'data-name']
+
+                }
             }
-        }
+        },
+
     },
     plugins: {
-        cleanDistFolderAndIndexfile: new CleanWebpackPlugin(["dist/*"])
+        cleanDistFolderAndIndexfile: new CleanWebpackPlugin(["dist/*"]),
+        buildHtmlIndex: new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        sassBuilder: sassBuilder
     }
 }
